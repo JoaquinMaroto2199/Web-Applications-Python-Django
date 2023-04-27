093fc0469a5dab827f7109daedb71a2b141c1035
Joaquin Maroto Pascual <joaquinmarpasrgmail.com>
2023-04-27 22:40:35 +0200

    Changes personal web

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from core import views
@@ -24,5 +25,5 @@ urlpatterns = [
     path('portafolio', views.portafolio, name= "portafolio"),
     path('contact', views.contacto, name = "contacto"),
     path("about", views.about, name = "about"),
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 ]
