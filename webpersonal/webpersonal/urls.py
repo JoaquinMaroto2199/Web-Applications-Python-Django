eb7dc7d0a2556e134be256cfca0088d77701d16c
Joaquin Maroto Pascual <joaquinmarpas@gmail.com>
2023-05-10 20:14:58 +0200

    Changes admin personal website

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -18,10 +18,16 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('contact', views.contact, name = "contact"),
     path("about", views.about, name = "about"),
     path("portfolio", views.portfolio, name = "portfolio"),
-    path('admin', admin.site.urls)
+    path('admin/', admin.site.urls)
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
