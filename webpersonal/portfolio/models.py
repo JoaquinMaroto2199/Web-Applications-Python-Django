db579f8ac3ef8a6f76005ae7de214960a848a38d
Joaquin Maroto Pascual <joaquinmarpas@gmail.com>
2023-05-11 22:03:06 +0200

    Completed personal website

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripcion")
     image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(null = True, blank = True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha Creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha Modificación")
 
--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from portfolio import models
 
 # Create your views here.
+def portfolio(request):
+    projects = models.Project.objects.all()
+    return render(request, "portfolio/portfolio.html", {"projects": projects})
